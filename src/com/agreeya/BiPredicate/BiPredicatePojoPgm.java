532057bc256c54d0fc0fc76ab1eb400a561ac3ca
svinothkumaryoung <vinchi.frnds@gmail.com>
2021-12-08 16:25:52 +0530

    New Changes

--- src/com/agreeya/BiPredicate/BiPredicatePojoPgm.java
+++ src/com/agreeya/BiPredicate/BiPredicatePojoPgm.java
@@ -9,16 +9,18 @@ import java.util.function.Consumer;
 
 public class BiPredicatePojoPgm {
     static StudentInfoModel sim=DataAccessLayer.getStudent();
-    static BiPredicate<String,String> bip=(name,country)->name.equals("Vinoth") && country.equals("India");
+    static BiPredicate<String,String> bip=(n,c)->n.equals("Vinoth") && c.equals("India");
 
     static Consumer<StudentInfoModel> si=(s)->{
       if(bip.test(s.getName(),s.getCountry()))
       {
           System.out.println("Condition Satisfied");
+          System.out.print(sim);
       }
       else
       {
           System.out.print("Condition Not Satisfied");
+          System.out.print(sim);
       }
     };
     public static void main(String[] args) {
